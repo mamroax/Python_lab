83ce6e8deac3024c1134ecde65b32f83cbca4bef
mamroax <lefrutite@gmail.com>
2022-09-13 23:07:54 +0300

    +

--- lab_2/versions_1-10.py
+++ lab_2/versions_1-10.py
@@ -37,7 +37,7 @@ def version_5(list1: [str]) -> sum:
     return summ
 
 
-def version_6(list1: list) -> int:
+def version_6(list1: list) -> tuple[int, int]:
     """Написать функцию, которая принимает список, состоящий
     из n элементов, и возвращает количество целых чисел и
     чисел с плавающей точкой"""
@@ -56,30 +56,59 @@ def version_7(list1: list[int]) -> None:
     список, состоящий из n элементов, и записывает в файл новый
     список, состоящий из тех элементов входящего списка,
     значения квадрата которых не превышают 30"""
+    try:
+        res_str = ''
+        for iterator in list1:
+            if iterator * iterator > 30:
+                break
+            else:
+                res_str += str(iterator) + ' '
+        my_file = open("list.txt", "w+")
+        my_file.write(res_str)
+    except Exception:
+        print('Error massage')
+    finally:
+        my_file.close()
+        print('Текстовый файл создан и заполнен')
 
 
-
-def version_8():
+def version_8(list1: list[int]) -> list[list[int]]:
     """Написать функцию, которая принимает целочисленный
     список, состоящий из n элементов, и возвращает список, состоящий
     из новых списков, элементами которых являются числа
     от 0 до значений элемента во входящем списке включительно.
     Например, входящий список имеет вид [1,2,3] на выходе будет
     список списков [[0,1], [0,1,2], [0,1,2,3]]"""
+    res_list = []
+    for iterator in list1:
+        res_list.append([i for i in range(iterator + 1)])
+    return res_list
 
 
-def version_9():
+def version_9(list1: list[int], num: int) -> None:
     """Написать функцию, которая принимает целочисленный
     список, содержащий n элементов, и целое число, и выводит на
     экран сгенерированные списки, содержащие n элементов с шагом
     итерации, равном элементу входящего списка"""
+    for iterator in list1:
+        print([jiterator for jiterator in range(0, iterator * num, iterator)])
 
 
-def version_10():
+def version_10(string1: str, string2: str) -> int:
     """Написать функцию, которая принимает на вход две
     разных строки и возвращает количество совпадений двух символов
     в строках. Например, на входе две строки «xadasw» и
     «xad» совпадением будет считаться группа символов «xa» и «ad»"""
+    first = 0
+    second = 2
+    counter = 0
+    for i in range(len(string1) - 1):
+        if string1[first:second] in string2:
+            # print(string1[first:second])
+            counter += 1
+        first += 1
+        second += 1
+    return counter
 
 
 print('Вариант 1')
@@ -96,3 +125,11 @@ print('Сумма чисел ', version_5(['12', '2.3', '123.23', '-100', '-12.4
 print('Вариант 6')
 print('Количество целых', version_6([12, 2.3, 123.23, -100, -12.4, 67, 7])[0], 'вещественных',
       version_6([12, 2.3, 123.23, -100, -12.4, 67, 7])[1])
+print('Вариант 7')
+version_7([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+print('Вариант 8')
+print(version_8([1, 2, 3, 4, 5]))
+print('Вариант 9')
+version_9([1, 2, 3, 4, 5, -6], 6)
+print('Вариант 10')
+print(version_10('xadasw', 'xad'))
