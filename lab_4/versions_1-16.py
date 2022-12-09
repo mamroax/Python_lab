15487875cffc96b1b49cb57cc8d1c6d1f4abe1fe
mamroax <mamroax@gmail.com>
2022-12-09 14:47:19 +0300

    6 labs left to do

--- lab_4/versions_1-16.py
+++ lab_4/versions_1-16.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from time import gmtime, strftime
 
@@ -82,26 +83,39 @@ def var10(gmt: str) -> datetime:
     print(type(strftime("%z", gmtime())))
 
 
-def var11():
+def var11(path: str) -> list[str]:
     """11. Написать функцию, которая принимает путь к директо-
     рии и возвращает список с наименованиями файлов, которые
     хранятся в полученной директории."""
+    return os.listdir(path)
 
 
-def var12():
+def var12(path: str, file_name: str) -> str:
     """12. Написать функцию, которая принимает путь к директо-
     рии и наименование файла и возвращает полный путь к полу-
     ченному файлу."""
+    return path + '/' + file_name
 
-def var13():
+
+def var13(list_of_path: list) -> int:
     """13. Написать функцию, которая принимает список путей и
     возвращает число путей, которые являются директорией."""
+    counter = 0
+    for element in list_of_path:
+        if '.' not in ('C:/Users/lefru/Desktop/python_labs/Python_lab/' + element):
+            counter += 1
+    return counter
 
-def var14():
+
+def var14(path: str):
     """14. Написать функцию, которая принимает путь к директо-
     рии и удаляет в ней все файлы, а вместо них создает директо-
     рии с тем же наименованием, что и удаленные файлы без зна-
     чения расширения."""
+    # сначала запомним все названия файлов в список
+    # теперь удалим все файлы без расширения
+    # создадим пустые папки с названиями фалов, у которых были расширения
+
 
 def var15():
     """15. Написать функцию относительно задания 8, которая
@@ -111,6 +125,8 @@ def var15():
     2019, внутри неё 09 и т.д. Стоит отметить, что значение другого
     объекта 2019-07-02 создаст в ранее созданной директории 2019
     директорию с наименованием 07."""
+
+
 
 def var16():
     """16. Написать функцию, которая принимает список списков
@@ -131,13 +147,13 @@ if __name__ == "__main__":
     print('Вариант 4 ', var4(datetime.today()))
     print('Вариант 5 ', var5(datetime.today()))
     print('Вариант 6 ', var6(datetime.now(), datetime(2021, 1, 1)))
-    print('Вариант 7 ', var7(datetime.now()))
-    print('Вариант 8 ', var8(14))
+    print('Вариант 7 ', var7(datetime.now())) # доделать
+    print('Вариант 8 ', var8(14)) # доделать
     print('Вариант 9 ', var9(datetime.now()))
-    print('Вариант 10 ', var10())
-    print('Вариант 11 ', var11())
-    print('Вариант 12 ', var12())
-    print('Вариант 13 ', var13())
+    print('Вариант 10 ', var10('3')) # доделать
+    print('Вариант 11 ', var11('C:/Users/lefru/Desktop/python_labs/Python_lab'))
+    print('Вариант 12 ', var12('C:/Users/lefru/Desktop/python_labs/Python_lab','README.md'))
+    print('Вариант 13 ', var13(var11('C:/Users/lefru/Desktop/python_labs/Python_lab')))
     print('Вариант 14 ', var14())
     print('Вариант 15 ', var15())
     print('Вариант 16 ', var16())
